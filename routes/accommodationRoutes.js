c6d792d620b74d884adb078c4ee7a9fff52cfa73
Alexa <alexandra.lundqvist1984@gmail.com>
2025-05-15 15:46:35 +0200

    accommodationRoutes kodad

--- routes/accommodationRoutes.js
+++ routes/accommodationRoutes.js
@@ -1,1 +1,24 @@
- 
+const express = require('express');
+const router = express.Router();
+const {
+  createAccommodation,
+  getAllAccommodations,
+  updateAccommodation,
+  deleteAccommodation
+} = require('../controllers/accommodationController');
+
+const verifyToken = require('../middleware/authMiddleware');
+const requireAdmin = require('../middleware/roleMiddleware');
+
+router.use(verifyToken); // Alla kräver inloggning
+
+// Alla får läsa
+router.get('/', getAllAccommodations);
+
+// Endast admin får skapa, ändra, ta bort
+router.post('/', requireAdmin, createAccommodation);
+router.put('/:id', requireAdmin, updateAccommodation);
+router.delete('/:id', requireAdmin, deleteAccommodation);
+
+module.exports = router;
+
