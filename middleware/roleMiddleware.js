26114b2388b4d97a687ea82364bfb33aa3c968b6
Alexa <alexandra.lundqvist1984@gmail.com>
2025-05-15 16:07:37 +0200

    roleMiddleware kodad

--- middleware/roleMiddleware.js
+++ middleware/roleMiddleware.js
@@ -1,1 +1,9 @@
- 
+const requireAdmin = (req, res, next) => {
+  if (req.user.role !== 'Admin') {
+    return res.status(403).json({ error: 'Endast admin har behörighet' });
+  }
+  next();
+};
+
+module.exports = requireAdmin;
+
