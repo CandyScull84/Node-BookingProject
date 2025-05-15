4d82da46f8f527fb88230f0901d6273568597b1e
Alexa <alexandra.lundqvist1984@gmail.com>
2025-05-15 17:27:01 +0200

    authController kodad

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,41 @@
- 
+const User = require('./models/User');
+const jwt = require('jsonwebtoken');
+
+// Registrera en ny användare
+const register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = new User({ username, password });
+    await user.save();
+
+    res.status(201).json({ message: 'Användare registrerad' });
+  } catch (err) {
+    res.status(400).json({ error: 'Registrering misslyckades', details: err.message });
+  }
+};
+
+// Logga in och få token
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(401).json({ error: 'Fel användarnamn eller lösenord' });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: '1d' }
+    );
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: 'Inloggning misslyckades', details: err.message });
+  }
+};
+
+module.exports = { register, login };
+
