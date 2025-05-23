a1aeb8e84e547ff0b754c1184776ab7fe382e838
CandyScull84 <din@email.com>
2025-05-23 20:31:15 +0200

    Ändrad socket

--- Hotell-frontend/src/utils/auth.js
+++ Hotell-frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-import jwtDecode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 
 // Hämta användaren från token
 export function getCurrentUser() {
--- Hotell-frontend/src/utils/socket.js
+++ Hotell-frontend/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.REACT_APP_SOCKET_URL || "http://localhost:8080";
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || "http://localhost:8080";
 // Byt URL till din backend-server om den inte körs lokalt
 
 const socket = io(SOCKET_URL, {
