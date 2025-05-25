3d096821cb97b48b89974d066e19573b33051c4d
CandyScull84 <din@email.com>
2025-05-25 18:33:03 +0200

    jwt i authcontroller uppdaterad

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,7 +26,7 @@ const login = async (req, res) => {
     }
 
     const token = jwt.sign(
-      { id: user._id, role: user.role },
+      { id: user._id, username: user.username, role: user.role },
       process.env.JWT_SECRET,
       { expiresIn: '1d' }
     );
