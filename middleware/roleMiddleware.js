2fe1cd67c372f544433b7380fe2bfbe09b67e34c
CandyScull84 <din@email.com>
2025-05-25 21:08:35 +0200

    roleMiddleware console.log

--- middleware/roleMiddleware.js
+++ middleware/roleMiddleware.js
@@ -1,4 +1,5 @@
 const requireAdmin = (req, res, next) => {
+   console.log('🛡️ Kontrollerar roll för:', req.user);
   if (req.user.role !== 'Admin') {
     return res.status(403).json({ error: 'Endast admin har behörighet' });
   }
