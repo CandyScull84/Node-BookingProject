b08541c19d3d3c525a9743dee9ee65e8898b52c2
CandyScull84 <din@email.com>
2025-05-25 20:42:04 +0200

    authroutes uppdaterat

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,41 +1,45 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
-const verifyToken = require('../middleware/authMiddleware');
-const User = require('../models/User');
-const requireAdmin = require('../middleware/roleMiddleware');
-const cache = require('../middleware/cacheMiddleware');
+const { register, login, getAllUsers, updateUserRole } = require('../controllers/authController');
+// const { register, login } = require('../controllers/authController');
+// const verifyToken = require('../middleware/authMiddleware');
+// const User = require('../models/User');
+// const requireAdmin = require('../middleware/roleMiddleware');
+// const cache = require('../middleware/cacheMiddleware');
 
 router.post('/register', register);
 router.post('/login', login);
 
-router.get('/all', verifyToken, requireAdmin, cache, async (req, res) => {
-  try {
-    const Users = await User.find();
-    res.json(Users);
-  } catch (err) {
-    res.status(500).json({ error: 'Kunde inte hämta användare' });
-  }
-});
+router.get('/all', verifyToken, requireAdmin, cache, getAllUsers);
 
-// routes/authRoutes.js
-router.patch('/role/:userId', verifyToken, requireAdmin, async (req, res) => {
-  const { userId } = req.params;
-  const { role } = req.body;
+router.patch('/role/:userId', verifyToken, requireAdmin, updateUserRole);
+// router.get('/all', verifyToken, requireAdmin, cache, async (req, res) => {
+//   try {
+//     const Users = await User.find();
+//     res.json(Users);
+//   } catch (err) {
+//     res.status(500).json({ error: 'Kunde inte hämta användare' });
+//   }
+// });
 
-  if (!['User', 'Admin'].includes(role)) {
-    return res.status(400).json({ error: 'Ogiltig roll' });
-  }
+// // routes/authRoutes.js
+// router.patch('/role/:userId', verifyToken, requireAdmin, async (req, res) => {
+//   const { userId } = req.params;
+//   const { role } = req.body;
 
-  try {
-    const user = await User.findByIdAndUpdate(userId, { role }, { new: true });
-    if (!user) return res.status(404).json({ error: 'Användare ej hittad' });
+//   if (!['User', 'Admin'].includes(role)) {
+//     return res.status(400).json({ error: 'Ogiltig roll' });
+//   }
 
-    res.json({ message: 'Roll uppdaterad', user });
-  } catch (err) {
-    res.status(500).json({ error: 'Misslyckades med att uppdatera roll' });
-  }
-});
+//   try {
+//     const user = await User.findByIdAndUpdate(userId, { role }, { new: true });
+//     if (!user) return res.status(404).json({ error: 'Användare ej hittad' });
+
+//     res.json({ message: 'Roll uppdaterad', user });
+//   } catch (err) {
+//     res.status(500).json({ error: 'Misslyckades med att uppdatera roll' });
+//   }
+// });
 
 module.exports = router;
 
