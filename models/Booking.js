10f6e8f22ffe58352a7157ad8677a2946465c5f7
CandyScull84 <din@email.com>
2025-06-12 12:47:16 +0200

    Booking update

--- models/Booking.js
+++ models/Booking.js
@@ -12,15 +12,31 @@ const bookingSchema = new mongoose.Schema({
     required: true, 
   },
   guests: { type: Number, required: true },
-  startDate: { type: Date, required: true },
-  endDate: { type: Date, required: true },
 
-   // För konferensrum:
-  date: { type: Date }, // t.ex. "2023-10-01"
-  startTime: { type: String }, // t.ex. "09:00"
-  endTime: { type: String }    // t.ex. "12:00"
-}, {
-  timestamps: true
-});
+  // För hotellrum
+  startDate: { 
+    type: Date,
+    required: function () {
+      return !this.date;
+    }
+  },
+  endDate: { 
+    type: Date,
+    required: function () {
+      return !this.date;
+    }
+  },
+
+  // För konferensrum
+  date: { 
+    type: Date,
+    required: function () {
+      return !this.startDate;
+    }
+  },
+  startTime: { type: String },
+  endTime: { type: String }
+
+}, { timestamps: true });
 
 module.exports = mongoose.model('booking', bookingSchema);
