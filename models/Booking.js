6fc7ea80100881f94d743affc40440e5bf7c976a
CandyScull84 <din@email.com>
2025-06-12 12:51:44 +0200

    booking update

--- models/Booking.js
+++ models/Booking.js
@@ -4,39 +4,51 @@ const bookingSchema = new mongoose.Schema({
   roomId: { 
     type: mongoose.Schema.Types.ObjectId, 
     ref: 'Rooms', 
-    required: true,
+    required: true
   },
   userId: { 
     type: mongoose.Schema.Types.ObjectId, 
     ref: 'User', 
-    required: true, 
+    required: true
   },
-  guests: { type: Number, required: true },
-
-  // För hotellrum
-  startDate: { 
-    type: Date,
-    required: function () {
-      return !this.date;
-    }
-  },
-  endDate: { 
-    type: Date,
-    required: function () {
-      return !this.date;
-    }
+  guests: { 
+    type: Number, 
+    required: true 
   },
 
-  // För konferensrum
-  date: { 
-    type: Date,
-    required: function () {
-      return !this.startDate;
-    }
-  },
-  startTime: { type: String },
-  endTime: { type: String }
+  // Hotellrum
+  startDate: Date,
+  endDate: Date,
+
+  // Konferensrum
+  date: Date,
+  startTime: String,
+  endTime: String
 
 }, { timestamps: true });
+
+// 🛡️ Anpassad validering för att skilja hotell och konferens
+bookingSchema.pre('validate', function (next) {
+  const isHotelBooking = this.startDate || this.endDate;
+  const isConferenceBooking = this.date || this.startTime || this.endTime;
+
+  if (isHotelBooking) {
+    if (!this.startDate || !this.endDate) {
+      return next(new Error('Både startDate och endDate krävs för hotellbokning'));
+    }
+  }
+
+  if (isConferenceBooking) {
+    if (!this.date || !this.startTime || !this.endTime) {
+      return next(new Error('Datum, start- och sluttid krävs för konferensbokning'));
+    }
+  }
+
+  if (!isHotelBooking && !isConferenceBooking) {
+    return next(new Error('Bokningen måste vara antingen hotell eller konferens'));
+  }
+
+  next();
+});
 
 module.exports = mongoose.model('booking', bookingSchema);
