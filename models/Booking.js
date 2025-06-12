c28636051310f9e4cf06ede68d4321e3743ebd5b
CandyScull84 <din@email.com>
2025-06-12 12:42:00 +0200

    Booking update Rooms

--- models/Booking.js
+++ models/Booking.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bookingSchema = new mongoose.Schema({
   roomId: { 
     type: mongoose.Schema.Types.ObjectId, 
-    ref: 'Room', 
+    ref: 'Rooms', 
     required: true,
   },
   userId: { 
